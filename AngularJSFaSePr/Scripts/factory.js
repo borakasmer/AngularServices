973d21831d6f9d2f5527e4e89bb3e3a78bae6364
Bora KAŞMER <bora@borakasmer.com>
2016-01-17 23:53:47 +0200

    Create Project

---
+++ AngularJSFaSePr/Scripts/factory.js
@@ -0,0 +1,26 @@
+﻿/// <reference path='angular.min.js' />
+
+var module = angular.module('app', []);
+module.factory('DropDownFactory', function () {
+    return {
+        getAllData: function () {
+            return [{ plaka: '01', il: 'ADANA' }, { plaka: '02', il: 'ADIYAMAN' }, { plaka: '03', il: 'AFYON' }, { plaka: '04', il: 'AĞRI' }, { plaka: '05', il: 'AMASYA' }, { plaka: '06', il: 'ANKARA' }, { plaka: '07', il: 'ANTALYA' }
+            , { plaka: '08', il: 'ARTVİN' }, { plaka: '09', il: 'AYDIN' }, { plaka: '10', il: 'BALIKESİR' }, { plaka: '11', il: 'BİLECİK' }, { plaka: '12', il: 'BİNGÖL' }, { plaka: '13', il: 'BİTLİS' }
+            , { plaka: "14", il: 'BOLU' }, { plaka: "15", il: 'BURDUR' }, { plaka: "16", il: 'BURSA' }, { plaka: "17", il: 'ÇANAKKALE' }, { plaka: "18", il: 'ÇANKIRI' }, { plaka: "19", il: 'ÇORUM' }, { plaka: "20", il: 'DENİZLİ' }
+            , { plaka: "21", il: 'DİYABAKIR' }, { plaka: "22", il: 'EDİRNE' }, { plaka: "23", il: 'ELAZIĞI' }, { plaka: "24", il: 'ERZİNCAN' }, { plaka: "25", il: 'ERZURUM' }, { plaka: "26", il: 'ESKİŞEHİR' }];
+        },
+        writePlaka: function (No) {            
+            return "Plaka :" + No;
+        }
+    }
+});
+
+module.controller('CityController', function ($scope, DropDownFactory) {
+    $scope.cityList = DropDownFactory.getAllData();
+    $scope.update = function () {        
+        $scope.Plaka = DropDownFactory.writePlaka($scope.selectedItem)
+    };
+    /*angular.forEach($scope.cityList, function (item) {
+        console.log(item);
+    });*/
+});
---
+++ AngularJSFaSePr/Scripts/friends.js
@@ -0,0 +1,85 @@
+﻿/// <reference path="angular.min.js" />
+var module = angular.module('app', []);
+module.service("FirendServices", function () {
+    var uid = 2;
+
+    var friendsList = [{
+        id: 0,
+        'name': 'Engin',
+        'email': 'engin@enginpolat.com',
+        'phone': '123-2343-44'
+    }, {
+        id: 1,
+        'name': 'Mehmet',
+        'email': 'mehmet@pakcamsil.com',
+        'phone': '222-3333-54'
+    },
+    {
+        id: 2,
+        'name': 'Caglar',
+        'email': 'caglar@caglarozenc.com',
+        'phone': '234-5421-78'
+    }
+    ];
+
+    this.save = function (friend) {
+        if (friend.id == null) {
+            friend.id = ++uid;
+            console.log(JSON.stringify(friend));
+            friendsList.push(friend);
+        }
+        else {
+
+            for (var i = 0; i < friendsList.length; i++) {
+                if (friendsList[i].id == friend.id) {
+                    friendsList[i] = friend;
+                }
+            };
+        }
+    }
+
+    this.get = function (id) {
+        var result=null;
+        angular.forEach(friendsList, function (frnd) {
+            if (frnd.id == id) {
+                result= frnd;                
+            }            
+        });
+        return result;
+    }
+
+    //iterate through contacts list and delete 
+    //contact if found
+    this.delete = function (id) {
+        angular.forEach(friendsList, function (frnd) {
+            if (frnd.id == id) {
+                friendsList.splice(friendsList.indexOf(frnd), 1);
+            }
+        })
+    }
+    this.list = function () {
+        return friendsList;
+    }
+});
+
+module.controller('FriendController', function ($scope, FirendServices) {
+
+    $scope.friends = FirendServices.list();
+
+    $scope.saveFriend = function () {
+        FirendServices.save($scope.newfriend);
+        $scope.newfriend = {};
+    }
+
+
+    $scope.delete = function (id) {
+
+        FirendServices.delete(id);
+        if ($scope.newfriend.id == id) $scope.newfriend = {};
+    }
+
+
+    $scope.edit = function (id) {
+        $scope.newfriend = angular.copy(FirendServices.get(id));       
+    }
+})
